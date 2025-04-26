05cb01bca88aa4bf2ef1fcf6137b7351a854d75f
Doggie1276 <immersivesim.ru@gmail.com>
2025-04-26 17:02:17 +0300

    Update 1.py
    2.1 version

--- 1.py
+++ 1.py
@@ -1,16 +1,24 @@
 from random import randint
 from time import sleep
+class Randomizer:
+    def __init__(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
+        self.result = self.__get_generated_value()
 
-def generatee(value1, value2):
-    return randint(value1, value2)
+    def __get_generated_value(self):
+        return randint(self.value1, self.value2)
+    
+    def get_info(self):
+        return f"Случайное число: {self.result}"
 
 a = int(input('Введите число от:'))
 b = int(input('Введите число до:'))
 
-result = generatee(a, b)
+rand = Randomizer(a, b)
 
 print(f"Подбираем число в диапозоне от {a} до {b}...")
 sleep(1)
 print(f"Секунду...")
 sleep(1)
-print(f"Случайное число: {result}")
+print(rand.get_info())
