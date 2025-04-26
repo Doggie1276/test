fab2e328bac91a442b3ad22f3565cb39806c3335
Doggie1276 <immersivesim.ru@gmail.com>
2025-04-26 16:49:47 +0300

    Update 1.py 2v
    2.0 version

--- 1.py
+++ 1.py
@@ -1,10 +1,13 @@
 from random import randint
 from time import sleep
 
+def generatee(value1, value2):
+    return randint(value1, value2)
+
 a = int(input('Введите число от:'))
 b = int(input('Введите число до:'))
 
-result = randint(a, b)
+result = generatee(a, b)
 
 print(f"Подбираем число в диапозоне от {a} до {b}...")
 sleep(1)
