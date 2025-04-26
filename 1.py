f33a7fd5dd2462580f40fe7ceeb0bfee9a496260
Doggie1276 <immersivesim.ru@gmail.com>
2025-04-26 16:42:40 +0300

    Update 1.py
    Генератор случайных чисел

--- 1.py
+++ 1.py
@@ -1,1 +1,13 @@
+from random import randint
+from time import sleep
 
+a = int(input('Введите число от:'))
+b = int(input('Введите число до:'))
+
+result = randint(a, b)
+
+print(f"Подбираем число в диапозоне от {a} до {b}...")
+sleep(1)
+print(f"Секунду...")
+sleep(1)
+print(f"Случайное число: {result}")
